1d80611010a1747bbe3e44e55cfd2de1830946ee
Aditya Kamath <a_kamath@hotmail.com>
2017-09-22 15:26:48 +0530

    Add account creation

---
+++ create_account.php
@@ -0,0 +1,38 @@
+<?php 
+	$db = mysqli_connect('mysql_host', 'mysql_user', 'mysql_password', 'mysql_db'); 
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	  echo "Err: " . mysqli_connect_error();
+	  exit;
+	}
+
+	// Strings must be escaped to prevent SQL injection attack. 
+	$name = mysql_real_escape_string($db, $_GET['User']); 
+	$pass = mysql_real_escape_string($db, $_GET['Pass']); 
+	$hash = $_GET['hash']; 
+
+	$secretKey = "farmersAreCool";
+
+	$real_hash = md5($name . $score . $secretKey); 
+	if($real_hash == $hash) 
+	{ 
+		// Send variables for the MySQL database class.
+		$pass = password_hash($pass);
+		$query = "INSERT INTO User ('Username', 'Password', 'Join_date', 'Currency') VALUES ('$name', '$pass', CURRENT_DATE, 0);"; 
+		$result = mysqli_query($db, $query); 
+		if (!result) 
+		{
+			echo "Err: " . $mysqli->error;
+			exit;
+		}
+		else
+		{
+			echo "Success";
+		}
+	}
+	else
+	{
+		echo "Err: Failed Hash";
+	}
+?>
