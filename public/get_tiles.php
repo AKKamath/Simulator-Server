eba6a1a0af3b806bbecdea991ea39b8ea4fcb3ce
Aditya Kamath <a_kamath@hotmail.com>
2017-10-09 18:32:27 +0530

    Fix errors with get_tiles

--- public/get_tiles.php
+++ public/get_tiles.php
@@ -10,15 +10,15 @@
 	$dbInfo = json_decode($dbInfo, true);
 	// Connect to server
 	$db = mysqli_connect($dbInfo["Host"], $dbInfo["User"], $dbInfo["Pass"], $dbInfo["DB"]);
-	$result = mysqli_query($db, "SELECT TileId, X, Y, Value, Type, Parent FROM Tile WHERE UserId = ". mysqli_real_escape_string($db, $_GET['Id']) . ";");
+	$result = mysqli_query($db, "SELECT UserId, TileId, X, Y, Value, Type, Parent FROM Tile WHERE UserId = ". mysqli_real_escape_string($db, $_GET['Id']) . ";");
 	if(mysqli_num_rows($result) == 0)
-		$result = mysqli_query($db, "SELECT TileId, X, Y, Value, Type, Parent FROM Tile WHERE UserId = -1;");
+		$result = mysqli_query($db, "SELECT UserId, TileId, X, Y, Value, Type, Parent FROM Tile WHERE UserId = -1;");
 	if (mysqli_num_rows($result) > 0) 
 	{
 		// output data of each row
-		while($row = mysqli_fetch($result)) 
+		while($row = mysqli_fetch_row($result)) 
 		{
-			foreach $row as $r
+			foreach($row as $r)
 				echo $r . " ";
 			echo "\n";
 		}
